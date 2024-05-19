0ce735a1b2c3918d87c2eee7fc35a5cc60dc915f
Jan Michel Pfeifer <michel.pfeifer@mni.thm.de>
2024-05-19 21:03:14 +0200

    implemented test method for weather analyzer class and method for finding the smallest temp spread with TDD. Created empty findDayWithSmallestTemperature method in WeatherAnalyzer

--- src/main/java/de/exxcellent/challenge/WeatherAnalyzer.java
+++ src/main/java/de/exxcellent/challenge/WeatherAnalyzer.java
@@ -1,4 +1,9 @@
 package de.exxcellent.challenge;
 
+import java.util.List;
+
 public class WeatherAnalyzer {
+    public String findDayWithSmallestTempSpread(List<String[]> weatherData) {
+
+    }
 }
---
+++ src/test/java/de/exxcellent/challenge/WeatherAnalyzerTest.java
@@ -0,0 +1,23 @@
+package de.exxcellent.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class WeatherAnalyzerTest {
+    @Test
+    public void findWeatherWithSmallestTempSpread(){
+        WeatherAnalyzer analyzer = new WeatherAnalyzer();
+        List<String[]> weatherData = Arrays.asList(
+                new String[]{"Day", "MxT", "MxN"},
+                new String[]{"1", "88", "59"},
+                new String[]{"2", "79", "63"},
+                new String[]{"3", "77", "55"}
+        );
+        String result = analyzer.findDayWithSmallestTempSpread(weatherData);
+        assertEquals("3", result);
+    }
+}
