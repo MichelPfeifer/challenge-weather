826a8b05bc45f612863ad2476ef4cb85be112923
Jan Michel Pfeifer <michel.pfeifer@mni.thm.de>
2024-05-19 20:35:31 +0200

    Changed initial test methods name for testing read of weather data and added similar method for football.csv

--- src/test/java/de/exxcellent/challenge/CsvReaderTest.java
+++ src/test/java/de/exxcellent/challenge/CsvReaderTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CsvReaderTest {
     @Test
-    public void testReadCsvFile() throws IOException {
+    public void testReadCsvWeatherFile() throws IOException {
         CsvReader csvReader = new CsvReader();
         List<String[]> csvData = csvReader.readCsvFile("src/main/resources/de/exxcellent/challenge/weather.csv");
 
@@ -17,6 +17,16 @@ public class CsvReaderTest {
         assertEquals("Day", csvData.get(0)[0]);
         assertEquals("MxT", csvData.get(0)[1]);
         assertEquals("1", csvData.get(1)[0]);
+    }
 
+    @Test
+    public void testReadCsvFootballFile() throws IOException{
+        CsvReader csvReader = new CsvReader();
+        List<String[]> csvData = csvReader.readCsvFile("src/main/resources/de/exxcellent/challenge/football.csv");
+
+        assertEquals(21, csvData.size());
+        assertEquals("Team", csvData.get(0)[0]);
+        assertEquals("Games", csvData.get(0)[1]);
+        assertEquals("Arsenal", csvData.get(1)[0]);
     }
 }
