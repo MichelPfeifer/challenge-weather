e8ada0c721e80fc79bb2f9cdc2574710e07e2108
Jan Michel Pfeifer <michel.pfeifer@mni.thm.de>
2024-05-19 22:08:03 +0200

    Created every necessary object in the main method an ran final maven verify to check for errors

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,8 @@
 package de.exxcellent.challenge;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -12,14 +15,23 @@ public final class App {
      * This is the main entry method of your program.
      * @param args The CLI arguments passed
      */
-    public static void main(String... args) {
+    public static void main(String... args) throws IOException {
 
-        // Your preparation code …
+        // Create reader
+        CsvReader csvReader = new CsvReader();
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        // Analyze weather file
+        List<String[]> weatherData = csvReader.readCsvFile("src/main/resources/de/exxcellent/challenge/weather.csv");
+        WeatherAnalyzer weatherAnalyzer = new WeatherAnalyzer();
+
+        String dayWithSmallestTempSpread = weatherAnalyzer.findDayWithSmallestTempSpread(weatherData);     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        // Analyze football data
+        List<String[]> footballData = csvReader.readCsvFile("src/main/resources/de/exxcellent/challenge/football.csv");
+        FootballAnalyzer footballAnalyzer = new FootballAnalyzer();
+
+        String teamWithSmallestGoalSpread = footballAnalyzer.findTeamWithSmallestGoalDifference(footballData); // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
