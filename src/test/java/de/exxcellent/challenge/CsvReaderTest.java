8b2a5eee73fe7ec0796b78332628c62d5de9934b
Jan Michel Pfeifer <michel.pfeifer@mni.thm.de>
2024-05-19 20:31:32 +0200

    Implemented test class for csv reader class and readCsv method with simple tests asserting correct reading of csv file

---
+++ src/test/java/de/exxcellent/challenge/CsvReaderTest.java
@@ -0,0 +1,22 @@
+package de.exxcellent.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CsvReaderTest {
+    @Test
+    public void testReadCsvFile() throws IOException {
+        CsvReader csvReader = new CsvReader();
+        List<String[]> csvData = csvReader.readCsvFile("src/main/resources/de/exxcellent/challenge/weather.csv");
+
+        assertEquals(31, csvData.size());
+        assertEquals("Day", csvData.get(0)[0]);
+        assertEquals("MxT", csvData.get(0)[1]);
+        assertEquals("1", csvData.get(1)[0]);
+
+    }
+}
